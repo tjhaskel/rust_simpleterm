7c8c5f2c78531bbcb0a949c9b59f05531505ba49
Trevan Haskell <dev@trevanhaskell.com>
2020-06-20 15:58:58 -0600

    display text

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
 use piston_window::*;
+use std::path::Path;
 
 fn main() {
-    let mut window: PistonWindow = WindowSettings::new("Hello Piston!", (640, 480))
+    let mut window: PistonWindow = WindowSettings::new(
+            "piston: hello_world",
+            [800, 600]
+        )
         .exit_on_esc(true)
         .build()
-        .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
+        .unwrap();
+
+    let resources: &Path = Path::new("resources");
+    let mut glyphs = window.load_font(resources.join("LeagueSpartan-Regular.ttf")).unwrap();
+
+    window.set_lazy(true);
     while let Some(e) = window.next() {
-        window.draw_2d(&e, |_c, g, _d| {
-            clear([0.5, 1.0, 0.5, 1.0], g);
+        window.draw_2d(&e, |c, g, device| {
+            let transform = c.transform.trans(10.0, 30.0);
+
+            clear([0.0, 0.0, 0.0, 1.0], g);
+            text::Text::new_color([0.0, 1.0, 0.0, 1.0], 32).draw(
+                "Hello world!",
+                &mut glyphs,
+                &c.draw_state,
+                transform, g
+            ).unwrap();
+
+            // Update glyphs before rendering.
+            glyphs.factory.encoder.flush(device);
         });
     }
 }
