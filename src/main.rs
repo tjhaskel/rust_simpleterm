327524e279b6281d8943aae79a77ec1ad5ba18a3
Trevan Haskell <dev@trevanhaskell.com>
2020-06-21 23:10:31 -0600

    simple typewriter

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use piston_window::*;
 use std::path::Path;
+use std::{thread, time};
 
 fn main() {
     let mut window: PistonWindow = WindowSettings::new(
@@ -13,14 +14,17 @@ fn main() {
     let resources: &Path = Path::new("resources");
     let mut glyphs = window.load_font(resources.join("LeagueSpartan-Regular.ttf")).unwrap();
 
-    window.set_lazy(true);
+    let text: &str = "Hello world!";
+    let end: usize = text.len();
+    let mut marker: usize = 1;
+
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g, device| {
             let transform = c.transform.trans(10.0, 30.0);
 
             clear([0.0, 0.0, 0.0, 1.0], g);
             text::Text::new_color([0.0, 1.0, 0.0, 1.0], 32).draw(
-                "Hello world!",
+                &text[..marker],
                 &mut glyphs,
                 &c.draw_state,
                 transform, g
@@ -29,5 +33,10 @@ fn main() {
             // Update glyphs before rendering.
             glyphs.factory.encoder.flush(device);
         });
+
+        if marker < end {
+            marker += 1;
+            thread::sleep(time::Duration::from_millis(10));
+        }
     }
 }
