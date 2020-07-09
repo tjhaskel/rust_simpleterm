8e77862d4af5455236560025746c94d35e5be5fc
Trevan Haskell <dev@trevanhaskell.com>
2020-07-09 12:15:49 -0600

    demo more colors

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,16 @@ fn main() {
     term.show("Welcome to Simpleterm!", Duration::from_secs(2));
     term.tell("This is pretty cool! Here's a really long word to test stuff: 0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
         \nThis should be on a new line!");
+
+    term.set_colors(OFF_WHITE, EMERALD);
     let input_recieved: String = term.ask("Enter some input: ");
+
     term.set_colors(OFF_WHITE, DARK_PURPLE);
     term.tell(&format!("You said: {}", input_recieved));
+
     term.set_colors(DARK_GREY, LIGHT_BLUE);
     term.display_art(MONA, Duration::from_secs(2));
+
+    term.set_colors(DARK_GREY, CRIMSON);
     term.tell("Thus concludes the demo!");
-    
 }
